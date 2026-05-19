13df51ee35e80474be7b9b26bb32173e9f22040c
jm2463 <jm2463@kent.ac.uk>
2026-05-19 12:21:52 +0100

    Added Manage Bookings UI with booking reload functionality

--- src/ui/ManageBookingsUI.java
+++ src/ui/ManageBookingsUI.java
@@ -4,7 +4,28 @@ import service.BookingService;
 import javax.swing.*;
 
 public class ManageBookingsUI extends JPanel {
-    private BookingService bookingService;
+
+    private JPanel bookingsPanel;
+
+    public void loadBookings() {
+
+        bookingsPanel.removeAll();
+
+        List<Booking> bookings =
+                BookingRepository.getBookings();
+
+        for (Booking booking : bookings) {
+
+            JPanel bookingCard =
+                    createBookingCard(booking);
+
+            bookingsPanel.add(bookingCard);
+        }
+
+        bookingsPanel.revalidate();
+
+        bookingsPanel.repaint();
+    }
     public ManageBookingsUI() {
         this.bookingService = bookingService;
     }
