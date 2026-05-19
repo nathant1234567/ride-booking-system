e45d369319fd2d3fdc4bc82352460fd2593a9587
jm2463 <jm2463@kent.ac.uk>
2026-05-19 11:37:25 +0100

    i manually added fake booking to see if my UI works

--- src/repository/BookingRepository.java
+++ src/repository/BookingRepository.java
@@ -8,6 +8,22 @@ import java.util.List;
 public class BookingRepository {
     private static List<Booking> bookings = new ArrayList<>();
 
+    static {
+
+        bookings.add(
+
+                new Booking(
+                        null,
+                        "London Heathrow",
+                        "Canterbury",
+                        25,
+                        2,
+                        new java.util.Date(),
+                        new java.util.Date()
+                )
+        );
+    }
+
     public static void addBooking(Booking booking) {
         bookings.add(booking);
         System.out.println("Booking added: " + booking);
