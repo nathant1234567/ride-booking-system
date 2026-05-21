a752ac5576e32f20354501a1a921515bdb6e00e1
Ibitola Omole <dioo2@kent.ac.uk>
2026-05-21 22:11:18 +0100

    Fix JUnit 4 assertion layout parameters in BookingTest

--- test/classes/BookingTest.java
+++ test/classes/BookingTest.java
@@ -1,12 +1,16 @@
 package classes;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+// --- REMOVE JUPITER PACKAGES AND ADD JUNIT 4 IMPORTS ---
+import org.junit.Test;
+import static org.junit.Assert.*;
+// ------------------------------------------------------
+
 import java.util.Date;
 
-class BookingTest {
+public class BookingTest { // Class must be public in JUnit 4
+
     @Test
-    void testBookingConstructorAndGetters() {
+    public void testBookingConstructorAndGetters() { // Must be public void
         User user = new User("Nathan Thompson", "njt38@kent.ac.uk", "0123456789");
         String destination = "London Heathrow (LHR)";
         String pickup = "Canterbury";
@@ -18,23 +22,25 @@ class BookingTest {
 
         Booking booking = new Booking(user, destination, pickup, lengthEstimate, passengers, amountOfLuggage, date, time);
 
-        assertEquals(user, booking.getUser(), "User should match");
-        assertEquals(destination, booking.getDestination(), "Destination should match");
-        assertEquals(pickup, booking.getPickupLocation(), "Pickup location should match");
-        assertEquals(passengers, booking.getNumberOfPassengers(), "Number of passengers should match");
-        assertEquals(date, booking.getDate(), "Date should match");
-        assertEquals(time, booking.getTime(), "Time should match");
+        // Fixed for JUnit 4: The custom error message string goes FIRST
+        assertEquals("User should match", user, booking.getUser());
+        assertEquals("Destination should match", destination, booking.getDestination());
+        assertEquals("Pickup location should match", pickup, booking.getPickupLocation());
+        assertEquals("Number of passengers should match", passengers, booking.getNumberOfPassengers());
+        assertEquals("Date should match", date, booking.getDate());
+        assertEquals("Time should match", time, booking.getTime());
     }
 
     @Test
-    void testSetters() {
+    public void testSetters() { // Must be public void
         User user = new User("John Doe", "john@example.com", "987654321");
         Booking booking = new Booking(user, "Old Dest", "Old Pickup", 56, 1, 5, new Date(), new Date());
-        
+
         String newDest = "Central London";
 
         booking.setDestination(newDest);
 
-        assertEquals(newDest, booking.getDestination(), "Destination should be updated via setter");
+        // Fixed for JUnit 4: The custom error message string goes FIRST
+        assertEquals("Destination should be updated via setter", newDest, booking.getDestination());
     }
 }
